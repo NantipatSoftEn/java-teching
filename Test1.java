b2a7adc56f5c142d24ee0008ac55442e1994d396
Army <keymasterviriya1150@gmail.com>
2019-04-12 18:05:46 +0700

    Test1

---
+++ Test1.java
@@ -0,0 +1,41 @@
+public class Test1 {
+
+    public static void main(String[] args) {
+        number_invsert();
+
+    }
+
+    public  static void number(){
+        for(int i=0; i<8 ; i++){
+            for(int j =1;j<=i;j++)
+                System.out.print(j);
+            System.out.println();
+        } 
+    }
+    /*
+    1
+    12
+    123
+    1234
+    12345
+    123456
+    1234567
+    */ 
+    public static void number_invsert(){
+        for(int i=7; i > 0 ; i--){
+            for(int j =1;j<=i;j++){
+                System.out.print(j);
+            }
+            System.out.println();
+        } 
+    }
+    /*
+    1234567
+    123456
+    12345
+    1234
+    123
+    12
+    1
+    */ 
+}
