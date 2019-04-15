1c1192ceb0e5df5414fe1ddccc22a0a717fac2c2
Army <keymasterviriya1150@gmail.com>
2019-04-15 09:45:59 +0700

    function task

---
+++ array/TestArray1.java
@@ -0,0 +1,38 @@
+public class Testarray1 {
+
+    public static void main(String[] args) {
+        int   arr =    [2,4,6,8,11,13,15,17,20,22,24,26,88,99,0,23];
+    }
+
+    public  static  int  display1(int  n){
+        // define function this here ...
+        return n;
+    }
+    
+    public static  void  display2(int n[]){
+        // define function this here ...
+    }
+
+    public static int countOddNumber(int n[]){
+        int count = 0;
+        // define function this here ...
+        return count;
+    }
+
+    public  static int countEventNumber(int n[]){
+        int  count = 0;
+        // define function this here ...
+        return  count;
+    }
+
+    public  static  int[] change_position (int n[]){
+        return n
+    }
+    
+    public static boolean is_prime(){
+        boolean   b  =  false;       
+         // define function this here ...
+        return b;
+    }
+
+}
