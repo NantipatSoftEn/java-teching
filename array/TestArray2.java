3b7dfcca127d43dca25b09f5132f32edc28cd61b
Army <keymasterviriya1150@gmail.com>
2019-04-18 08:54:00 +0700

    maxtic

---
+++ array/TestArray2.java
@@ -0,0 +1,39 @@
+public class Testarray1 {
+
+    public static void main(String[] args) {
+        int   arr2d =    new int[3][3];
+        int  arr2dSet1 =  assign_value(arr2d);
+        int  arr2dSet2 =  assign_value(arr2d);
+
+
+    }
+
+    public static  int[][] assign_value(int arr[][]){
+        /*
+            pls assign value all  element in array 2  dimension
+            example  
+            n[0][0]  =   0+0
+            n[0][1]  =    0+1
+            n[2][2]  =   2+2
+        */
+    } 
+
+    public static  show_value(int n){
+        /*
+        n is parameter  from element array 2 d and return value 
+        */ 
+        return n
+    }
+
+    public  static plus_matrix(int set1[][],int set2[][]){
+        //https://www.varsitytutors.com/hotmath/hotmath_help/topics/adding-and-subtracting-matrices
+        int result[][] = new int[3][3];
+        return result;
+    }
+
+    public  static multiply_matrix(int set1[][],int set2[][]){
+        // https://www.google.com/search?q=multiply+matrix+3x3&source=lnms&tbm=isch&sa=X&ved=0ahUKEwirq6vhw9jhAhVELo8KHReZBrwQ_AUIDigB&biw=1366&bih=667#imgrc=uiH8lL_i7dQVgM:
+        int result[][] = new int[3][3];
+        return result;
+    }
+}
